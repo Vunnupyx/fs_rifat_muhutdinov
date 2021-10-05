b6272822d32eb91c212894931c7eacaa0f4c100e
raba0920 <rafael.badykov@netcracker.com>
2021-10-05 08:57:16 +0400

    Review.

--- Основы angular/searchForPets/src/app/app.component.ts
+++ Основы angular/searchForPets/src/app/app.component.ts
@@ -8,6 +8,9 @@ import {Animal, DataService} from './shared/data.service';
 })
 
 export class AppComponent {
+  /*todo: насчёт форматирования не забывай. Тут так же есть модификаторы доступа
+  *  Если переменная писпользуется только в классе, то сделай приватным, а если
+  *  она используется ещё и в шаблоне, то перед именем "_"*/
   searchText = '';
   animals: Animal[];
   expandedElement = -1;
@@ -15,6 +18,7 @@ export class AppComponent {
 
   hideCats() {
     if (this.hideItem) {
+      /* todo: использовать "it" в качестве имени - круто, но не читаемо, лучше просто "animal" и пропиши тип*/
       this.animals = this.animals.filter(it => {
         return it.breed !== 'Котик'
       });
@@ -29,5 +33,5 @@ export class AppComponent {
   ngOnInit() {
     this.animals = this.dataService.getAnimals()
   }
-
+  /*todo: Форматни все файлы, можно выделить через шифт и зажать ctrl+alt+L*/
 }
